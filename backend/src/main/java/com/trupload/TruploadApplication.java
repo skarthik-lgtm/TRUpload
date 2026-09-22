bdbd6e5d4f9005f759dd985f116405fed3bb9493
skarthik-lgtm <skarthik@cswg.com>
2026-09-22 23:10:30 +0530

    made changes

--- backend/src/main/java/com/trupload/TruploadApplication.java
+++ backend/src/main/java/com/trupload/TruploadApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TRUploadApplication { 
+public class TruploadApplication { 
 
     public static void main(String[] args) {
-        SpringApplication.run(TRUploadApplication.class, args);
+        SpringApplication.run(TruploadApplication.class, args);
     }
 }
